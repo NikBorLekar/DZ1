8c6bf158662b25f579687f4c035afc115feec3e2
Nik <126783560+NikBorLekar@users.noreply.github.com>
2023-03-13 12:26:36 +0700

    task8

---
+++ Program-task8.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int num1 = 2;
+
+if(num > 1)
+{
+    while(num1 <= num)
+    {
+        Console.Write(num1 + " ");
+        num1 = num1 + 2;
+    }
+}
