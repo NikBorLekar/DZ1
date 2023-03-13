40b848a716f70841c312f3e8d30cf7f194ed7b5a
Nik <126783560+NikBorLekar@users.noreply.github.com>
2023-03-13 12:26:12 +0700

    task6

---
+++ Program-task6.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int Div = num % 2;
+
+if(Div == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
