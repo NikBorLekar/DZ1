5a04b0e81ebc72e07882c48b560d92703b7a5866
nik <lekares@gmail.com>
2023-03-13 12:16:44 +0700

    task2

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int max = 0;
+int min = 0;
+if(num1 > num2)
+{
+    max = num1;
+    min = num2;
+}
+else
+{
+    max = num2;
+    min = num1;
+}
+Console.WriteLine("Максимальное значение = " + max + ", а " + "Минимальное значение = " + min);
